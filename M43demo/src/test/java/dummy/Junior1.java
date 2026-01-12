51cb111f76a9cd7ff0b2b44fa90773655cf9e39c
meghana <meghana@localhost>
2026-01-12 07:52:08 +0530

    junior1

---
+++ M43demo/src/test/java/dummy/Junior1.java
@@ -0,0 +1,10 @@
+package dummy;
+
+public class Junior1 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("hi i push the junior1 workspace");
+	}
+
+}
---
+++ M43demo/src/test/java/dummy/Sample.java
@@ -0,0 +1,13 @@
+package dummy;
+
+public class Sample {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+System.out.println("hi everyone");
+System.out.println("hi everyone");
+System.out.println("hi everyone");
+System.out.println("hi everyone");
+	}
+
+}
