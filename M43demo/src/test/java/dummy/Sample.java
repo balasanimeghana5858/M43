3055bf89afa7b8870d892a481e62aa0f0a4c6fe3
meghana <meghana@localhost>
2026-01-16 08:00:46 +0530

    juniors

--- M43demo/src/test/java/dummy/Sample.java
+++ M43demo/src/test/java/dummy/Sample.java
@@ -4,7 +4,10 @@ public class Sample {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-
+System.out.println("hi everyone");
+System.out.println("hi everyone");
+System.out.println("hi everyone");
+System.out.println("hi everyone");
 	}
 
 }
